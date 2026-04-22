a4d4a1680732b61fc6b981b2bd5c40f8281cd6d6
vischicarlosmario-svg <vischicarlosmario@gmail.com>
2026-04-22 14:52:43 -0300

    se coloco el codigo necesario para la importacion de error404

--- tecno/src/App.jsx
+++ tecno/src/App.jsx
@@ -6,6 +6,7 @@ import DetalleDeProducto from "./components/views/Producto/DetalleDeProducto"
 import Login from "./components/views/Login"
 import Administrador from "./components/views/Administrador"
 import FormularioProducto from "./components/views/Producto/FormularioProducto"
+import Error404 from "./components/views/Error404"
 import './App.css'
 
 function App() {
