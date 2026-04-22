93712e6ca086e618f4383f026c2b7b608a91b6c6
vischicarlosmario-svg <vischicarlosmario@gmail.com>
2026-04-22 14:11:59 -0300

    se importo el componente DetalleDeProducto

--- tecno/src/App.jsx
+++ tecno/src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route } from "react-router"
 import Menu from "./components/shared/Menu"
 import Footer from "./components/shared/Footer"
 import Home from "./components/views/Home"
+import DetalleDeProducto from "./components/views/Producto/DetalleDeProducto"
 import './App.css'
 
 function App() {
