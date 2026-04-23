5f6240608fcaa7a992b4c404d35d0a7e57dea822
vischicarlosmario-svg <vischicarlosmario@gmail.com>
2026-04-23 11:23:03 -0300

    se hizo la importacion de sweetalet2 junto con Swal

--- tecno/src/components/views/Login.jsx
+++ tecno/src/components/views/Login.jsx
@@ -1,6 +1,7 @@
 import { Card, Button, Row, Col ,Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import {useNavigate} from "react-router";
+import  Swal from "sweetalert2"
 
 
 
