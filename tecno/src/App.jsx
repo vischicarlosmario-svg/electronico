15cf44c6bef77d39bda8a2d07f681e609876d9f6
vischicarlosmario-svg <vischicarlosmario@gmail.com>
2026-04-22 23:47:28 -0300

    se utilizo el useState para usuarioLogueado

--- tecno/src/App.jsx
+++ tecno/src/App.jsx
@@ -13,6 +13,7 @@ import './App.css'
 
 function App() {
   const sesionUsuario = JSON.parse (sessionStorage.getItem("usuarioKey")) || false;
+  const [usuarioLogeado, setUsuarioLogueado] = useState(sesionUsuario);
   
 
 
