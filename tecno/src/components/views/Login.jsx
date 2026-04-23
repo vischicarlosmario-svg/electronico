c84ce16f43f19455c048fbe5b6ba1b9d07c33098
vischicarlosmario-svg <vischicarlosmario@gmail.com>
2026-04-23 19:18:44 -0300

    se estructuro un formulario de control

--- tecno/src/components/views/Login.jsx
+++ tecno/src/components/views/Login.jsx
@@ -1,13 +1,13 @@
-import { Card, Button, Row, Col ,Form } from "react-bootstrap";
+import { Card, Button, Row, Col, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
-import  Swal from "sweetalert2";
+import Swal from "sweetalert2";
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/Ai';
 import { useState } from "react"
 
-const Login = ({ setUsuarioLogueado}) => {
-  const [ showPassword, setShowPassword ] = useState(false);
-  const handlePassword = () => { setShowPassword(!showPassword)}
+const Login = ({ setUsuarioLogueado }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handlePassword = () => { setShowPassword(!showPassword) }
   const {
     register,
     handelerSubmit,
@@ -16,48 +16,48 @@ const Login = ({ setUsuarioLogueado}) => {
 
   const navigacion = useNavigate()
 
-  const onSubmit = (data)=>{
+  const onSubmit = (data) => {
     if (
       data.email === import.meta.env.VITE_API_EMAIL &&
       data.pasword === import.meta.env.VITE_API_PASSWORD
     )
   } {
-    console.log('Aqui logueo al usuario');
-    setUsuarioLogueado(true);
-    Swal.fire({
-      title: "Bienvenido Sr Administrador",
-      text: "Iniciando sesion correctamente",
-      icon: "success",
-    });
-    navegacion("/administrador");
-  } else {
-    Swal.fire({
-      title: "Ocurrio un error, intente nuevamente",
-      text: "Credenciales incorrectas",
-      icon: "error",
-    });
+      console.log('Aqui logueo al usuario');
+  setUsuarioLogueado(true);
+  Swal.fire({
+    title: "Bienvenido Sr Administrador",
+    text: "Iniciando sesion correctamente",
+    icon: "success",
+  });
+  navegacion("/administrador");
+} else {
+  Swal.fire({
+    title: "Ocurrio un error, intente nuevamente",
+    text: "Credenciales incorrectas",
+    icon: "error",
+  });
   }
 
 }
 
 return (
   <>
-  <Card className="shadow p-3 mb-5 bg-body rounded card-login">
-    <Row xs={1} md={2}>
-      <Col>
-        <Card.body>
-          <h1 className="text-center mb-4">Iniciar secion</h1>
-          <Form onSubmit={habdSubmit(onSubmit)}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email:</Form.Label>
+    <Card className="shadow p-3 mb-5 bg-body rounded card-login">
+      <Row xs={1} md={2}>
+        <Col>
+          <Card.body>
+            <h1 className="text-center mb-4">Iniciar secion</h1>
+            <Form onSubmit={habdSubmit(onSubmit)}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control>
+              </Form.Group>
 
-            </Form.Group>
-
-          </Form>
-        </Card.body>
-      </Col>
-    </Row>
-  </Card>
+            </Form>
+          </Card.body>
+        </Col>
+      </Row>
+    </Card>
   </>
 )
 
@@ -73,7 +73,7 @@ return (
 const Login = () => {
   return (
     <div>
-      
+
     </div>
   )
 }
