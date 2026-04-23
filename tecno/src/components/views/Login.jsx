cd5c58be7dabf38b2960612303fe18893013858d
vischicarlosmario-svg <vischicarlosmario@gmail.com>
2026-04-23 11:26:21 -0300

    se hizo la impportacion de react-icons para poder usar el boton de invisibilidad de las claves

--- tecno/src/components/views/Login.jsx
+++ tecno/src/components/views/Login.jsx
@@ -1,7 +1,8 @@
 import { Card, Button, Row, Col ,Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import {useNavigate} from "react-router";
-import  Swal from "sweetalert2"
+import  Swal from "sweetalert2";
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/Ai';
 
 
 
