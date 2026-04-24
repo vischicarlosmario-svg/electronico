63163c2c9ee6d9ff95861ea0abe8e42bead7f80d
Mariano Juarez <nanito.mj@gmail.com>
2026-04-24 00:15:53 -0300

    se añadieron variables al componente home y productos al componente productosPrueba.jsx

--- tecno/src/data/productosPrueba.jsx
+++ tecno/src/data/productosPrueba.jsx
@@ -2,15 +2,16 @@ const productos = [
     {
         id: "1",
         nombreProducto: "Minecraft",
-        precio: "$30USD",
+        precio: "$30 USD",
         descripcion_breve: "Minecraft es un videojuego de construcción de tipo «mundo abierto» o en inglés sandbox creado originalmente por el sueco Markus Persson, que creó posteriormente Mojang Studios",
         descripcion_amplia: "CREA, Crea lo que sea que puedas imaginar en tu propio mundo infinito que es único cada vez que lo juegues.",
         categoria: "Sandbox, Aventuras, Casual, Multijugador",
         imagen: "x"
     },
     {
-        id: "2"
-        nombreProducto: "Battlefield™ 6"
+        id: "2",
+        nombreProducto: "Battlefield™ 6",
+        precio: "$69.99 USD"
 
     }
 ];
