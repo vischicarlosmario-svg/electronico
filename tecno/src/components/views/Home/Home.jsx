d07e5491570cb4ee30b596c36da494a8c9346b19
Mariano Juarez <nanito.mj@gmail.com>
2026-04-24 00:14:03 -0300

    se añadieron cosas

--- tecno/src/components/views/Home/Home.jsx
+++ tecno/src/components/views/Home/Home.jsx
@@ -1,8 +1,21 @@
+import { StrictMode, useState } from 'react'
+import { createRoot } from 'react-dom/client'
+import './App.css'
+import App from './App.jsx'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 const Home = () => {
+
+  const [busqueda, setBusqueda] = useState();
+
+  const listaDeJuegos = []
+
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   )
 }
 
