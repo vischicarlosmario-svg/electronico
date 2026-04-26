43d4d7a66862da690df98619d219ac88d28dd22f
vischicarlosmario-svg <vischicarlosmario@gmail.com>
2026-04-26 11:37:36 -0300

    se hizio una importacion de lista de productos desde helpers/queries

--- tecno/src/components/views/Administrador.jsx
+++ tecno/src/components/views/Administrador.jsx
@@ -2,6 +2,7 @@ import { Button, Table } from "react-bootstrap"
 import { Link } from "react-router"
 import ItemProducto from "./Producto/ItemProducto"
 import productotosPrueba from "../../data/productosPrueba"
+import { listaProductos } from "../../helpers/queries"
 const Administrador = () => {
   return (
     <div>
