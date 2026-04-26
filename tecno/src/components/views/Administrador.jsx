d719b7b2f043d05f05320b51e11d76542ac6d0e3
vischicarlosmario-svg <vischicarlosmario@gmail.com>
2026-04-26 11:19:24 -0300

    se hizo la importacion de item producto a travez de el enlace de producto-itemproducto

--- tecno/src/components/views/Administrador.jsx
+++ tecno/src/components/views/Administrador.jsx
@@ -1,5 +1,6 @@
 import { Button, Table } from "react-bootstrap"
 import { Link } from "react-router"
+import ItemProducto from "./Producto/ItemProducto"
 const Administrador = () => {
   return (
     <div>
