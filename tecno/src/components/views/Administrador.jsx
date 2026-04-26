3384a0b61660482e88bd028cd1d215bb12c1f9f1
vischicarlosmario-svg <vischicarlosmario@gmail.com>
2026-04-26 11:39:15 -0300

    se hizo la importacion de Swal para colocar carteles de advertencia

--- tecno/src/components/views/Administrador.jsx
+++ tecno/src/components/views/Administrador.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router"
 import ItemProducto from "./Producto/ItemProducto"
 import productotosPrueba from "../../data/productosPrueba"
 import { listaProductos } from "../../helpers/queries"
+import Swal from "sweetalert2"
 const Administrador = () => {
   return (
     <div>
