3e3a38855b1a4afd6b6e05abef344435d209b85f
vischicarlosmario-svg <vischicarlosmario@gmail.com>
2026-04-26 11:35:34 -0300

    se importo productos prueba desde data

--- tecno/src/components/views/Administrador.jsx
+++ tecno/src/components/views/Administrador.jsx
@@ -1,6 +1,7 @@
 import { Button, Table } from "react-bootstrap"
 import { Link } from "react-router"
 import ItemProducto from "./Producto/ItemProducto"
+import productotosPrueba from "../../data/productosPrueba"
 const Administrador = () => {
   return (
     <div>
