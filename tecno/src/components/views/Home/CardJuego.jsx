10cb060089246c92ef7bea6401f4aa0be60668d1
Mariano Juarez <nanito.mj@gmail.com>
2026-04-25 23:19:46 -0300

    se añadio el componente JuegosPopulares al componente Home

--- tecno/src/components/views/Home/CardJuego.jsx
+++ tecno/src/components/views/Home/CardJuego.jsx
@@ -4,7 +4,7 @@ const CardJuego = ({nombreProducto, precio, imagen}) => {
   return (
     <>
       <div className="card h-100"> 
-        <img src={juego.imagen} alt="" />
+        <img src={juego.imagen} alt={juego.nombreProducto} />
         <div className="card-body">
             <h6 className="card-title">{juego.nombreProducto}</h6>
             <p className="card-text">{juego.precio}</p>
--- tecno/src/components/views/Home/Home.jsx
+++ tecno/src/components/views/Home/Home.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Carrousel from './Carrousel/Carrousel.jsx'
+import JuegosPopulares from './JuegosPopulares.jsx'
 
 const Home = () => {
 
