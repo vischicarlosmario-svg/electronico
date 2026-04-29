ec3cf974d00b92ac63cd4dd5a9c8b33b3936680d
Mariano Juarez <nanito.mj@gmail.com>
2026-04-29 16:48:27 -0300

    se creo funcion onClick para que las cards te redirijan a DetalleDeProducto

--- tecno/src/components/views/Home/cards/CardJuego.jsx
+++ tecno/src/components/views/Home/cards/CardJuego.jsx
@@ -1,11 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ProductosPrueba from '../../../../data/ProductosPrueba'
+import { useNavigate } from "react-router"
+
 const CardJuego = ({nombreProducto, precio, imagen}) => {
+
+  const navegar = useNavigate();
+  const detalleProducto = () => {
+    navegar(`/DetalleDeProducto/`);
+    console.log("click detectado redirigiendo a detalleProducto");
+    
+  }
+
   return (
     //modelo de card para usarse en los productos
     <>
     <div className="">
-      <div className="card"> 
+      <div onClick={detalleProducto} className="card" > 
         <img src={imagen} alt={nombreProducto} />
         <div className="card-body">
             <h6 className="card-title text-white">{nombreProducto}</h6>
