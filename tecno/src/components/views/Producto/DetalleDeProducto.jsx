4b02cd4b50d2f2625191aaaa51dc903da9c39939
Mariano Juarez <nanito.mj@gmail.com>
2026-04-29 16:48:59 -0300

    se creo la estructura de DetalleDeProducto

--- tecno/src/components/views/Producto/DetalleDeProducto.jsx
+++ tecno/src/components/views/Producto/DetalleDeProducto.jsx
@@ -1,23 +1,36 @@
 import { Container, Row, Col, Card } from "react-bootstrap"
 import ProductosPrueba from "../../../data/ProductosPrueba"
 
+import { Container, Row, Col, Card } from "react-bootstrap"
+
 const DetalleDeProducto = () => {
   return (
-    <>
-      {/* <Container>
-        <Card>
-          <Row>
-            <Col md={6}>
-            <Card.img
-            variant="top"
-            src={}
-            alt=""
+     <Container className="my-3 mainSection cabin-sketch-regular">
+      <Card>
+        <Row>
+          <Col md={6}>
+            <Card.Img
+              variant="top"
+              src='https://images.pexels.com/photos/2619970/pexels-photo-2619970.jpeg'
+              alt='pizza'
             />
-            </Col>
-          </Row>
-        </Card>
-      </Container> */}
-    </>
+          </Col>
+          <Col md={6}>
+            <Card.Body>
+              <Card.Title className="fs-2">Pizza Margarita Clásica</Card.Title>
+              <hr />
+              <Card.Text className="fs-5">
+              Nuestra pizza margarita artesanal hecha con masa madre, salsa de tomate casera, queso mozzarella de búfala premium y hojas frescas de albahaca. Horneada en horno de piedra para obtener ese crujiente perfecto.
+              <br/>
+              <br/>
+              <span className="fw-semibold ">Categoria:</span> comida
+              <br className='mb-3'/>
+              <span className="fw-semibold ">Precio: $4500</span></Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </Container>
   )
 }
 
