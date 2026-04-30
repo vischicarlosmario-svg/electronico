1544b634dbd7035aa175b1174a4560692ed82773
Mariano Juarez <nanito.mj@gmail.com>
2026-04-30 10:21:21 -0300

    se creo la estructura de ItemProducto

--- tecno/src/components/views/Producto/ItemProducto.jsx
+++ tecno/src/components/views/Producto/ItemProducto.jsx
@@ -1,9 +1,26 @@
-const ItemProducto = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { Button } from "react-bootstrap";
+import { Link } from "react-router";
 
-export default ItemProducto
+const ItemProducto = ({itemProducto}) => {
+  return (
+    <tr>
+      <td className="text-center">Fila</td>
+      <td>{itemProducto.nombreProducto}</td>
+      <td className="text-end">{itemProducto.precio}</td>
+      <td className="text-center">
+        <img src={itemProducto.imagen} className="img-thumbnail w-25" alt={itemProducto.imagen}/>
+      </td>
+      <td>{itemProducto.categoria}</td>
+      <td className="text-center">
+        <Link className="me-lg-2 btn btn-warning" to={`/administrador/editar/`}>
+          <i className="bi bi-pencil-square"></i>
+        </Link>
+        <Button variant="danger">
+          <i className="bi bi-trash"></i>
+        </Button>
+      </td>
+    </tr>
+  );
+};
+
+export default ItemProducto;
