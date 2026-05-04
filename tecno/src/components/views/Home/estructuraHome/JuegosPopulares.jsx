26a2a5d0e7815283078d77820dd5eabf77496fdf
facundoheredia17 <facundoheredia951@gmail.com>
2026-05-04 16:51:35 -0300

    decoracion con css

--- tecno/src/components/views/Home/estructuraHome/JuegosPopulares.jsx
+++ tecno/src/components/views/Home/estructuraHome/JuegosPopulares.jsx
@@ -8,7 +8,6 @@ const JuegosPopulares = ({nombreProducto, precio, imagen}) => {
     <>
     <div className="juegosPopulares container-fluid">
         <div className="juegosTitulo ">
-            <h2>Juegos populares</h2>
             <div className="row justify-content-center">
                 {ProductosPrueba.slice(0,5).map((juego) => (
                 <div className="col-6 col-md-4 col-lg-2 mb-3"key={juego.id}>
