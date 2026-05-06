049e1a0799bec1017f958de268db95b9b5f7be1e
Mariano Juarez <nanito.mj@gmail.com>
2026-05-06 04:58:52 -0300

    se maqueto la lista de usuarios en el panel de administrador

--- tecno/src/components/views/Producto/ItemUsuario.jsx
+++ tecno/src/components/views/Producto/ItemUsuario.jsx
@@ -1,9 +1,11 @@
 import { Table, Button } from "react-bootstrap"
 import Swal from "sweetalert2"
 
+const ItemUsuario = ({ usuario, fila, usuarios, setUsuarios }) => {
+
 const borrarUsuario = (emailBorrar) => {
     if (emailBorrar === import.meta.env.VITE_API_EMAIL) {
-        Swal.fire("Acceso denegado", "no puedes eliminar la cuenta de un administrador", "error")
+        return Swal.fire("Acceso denegado", "no puedes eliminar la cuenta de un administrador", "error")
     }
     Swal.fire({
         title: `Borrar usuario ${emailBorrar}`,
@@ -11,7 +13,7 @@ const borrarUsuario = (emailBorrar) => {
         icon: "warning",
         showCancelButton: true,
         confirmButtonText: "si"
-    }).themn((result) => {
+    }).then((result) => {
         if (result.isConfirmed) {
             const nuevosUsuarios = usuarios.filter((u) => u.email !== emailBorrar);
             setUsuarios(nuevosUsuarios)
@@ -20,3 +22,20 @@ const borrarUsuario = (emailBorrar) => {
         }
     })
 }
+
+return (
+    <>
+    <tr>
+        <td>{fila}</td>
+        <td>{usuario.nombre}</td>
+        <td>{usuario.email}</td>
+        <td className="text-center">
+            <Button variant="danger" size="sm" onClick={() => borrarUsuario(usuario.email)}>Borrar usuario</Button>
+        </td>
+    </tr>
+    </>
+
+
+    )
+}
+export default ItemUsuario
