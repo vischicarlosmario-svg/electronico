afa77d5911a172d22f2365f247b5229098d5dd10
Mariano Juarez <nanito.mj@gmail.com>
2026-05-06 02:57:48 -0300

    Se creo la logica para eliminar usuarios y la logica para que no se pueda borrar el rol de admin de ninguna manera

--- tecno/src/components/views/Producto/ItemUsuario.jsx
+++ tecno/src/components/views/Producto/ItemUsuario.jsx
@@ -1,2 +1,22 @@
 import { Table, Button } from "react-bootstrap"
 import Swal from "sweetalert2"
+
+const borrarUsuario = (emailBorrar) => {
+    if (emailBorrar === import.meta.env.VITE_API_EMAIL) {
+        Swal.fire("Acceso denegado", "no puedes eliminar la cuenta de un administrador", "error")
+    }
+    Swal.fire({
+        title: `Borrar usuario ${emailBorrar}`,
+        text: "El usuario ya no podra iniciar sesion",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "si"
+    }).themn((result) => {
+        if (result.isConfirmed) {
+            const nuevosUsuarios = usuarios.filter((u) => u.email !== emailBorrar);
+            setUsuarios(nuevosUsuarios)
+            localStorage.setItem("usuarios", JSON.stringify(nuevosUsuarios))
+            Swal.fire("Eliminado", "Usuario borrado correctamente", "success")
+        }
+    })
+}
